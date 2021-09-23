1de3b7c854ddb60d76bbc221d9d41ecd93979858
AnastasiiaSergienko <Anastasiia.Sergienko@exasol.com>
2021-09-23 13:42:18 +0200

    * #11: Added swagger.

--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import exasol_rest_api "main/pkg/exasol-rest-api"
-
-func main() {
-	exasol_rest_api.Run()
-}
--- pkg/exasol-rest-api/application.go
+++ pkg/exasol-rest-api/application.go
@@ -2,6 +2,10 @@ package exasol_rest_api
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "main/doc/swagger"
 )
 
 // Run starts the REST API service.
@@ -12,6 +16,8 @@ func Run() {
 	}
 	router := gin.Default()
 	router.GET("/api/v1/query/:query", application.Query)
+	swaggerURL := ginSwagger.URL("/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerURL))
 	err := router.Run(applicationProperties.ApplicationServer)
 	if err != nil {
 		panic("error starting API server: " + err.Error())
